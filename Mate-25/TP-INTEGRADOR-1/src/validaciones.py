4151952ab0a0022b869af79971766f102600fec1
Luis Cisneros <lcisneros@nacionservicios.com.ar>
2025-04-15 19:48:24 -0300

    lil change

---
+++ Mate-25/TP-INTEGRADOR-1/src/validaciones.py
@@ -0,0 +1,21 @@
+
+
+# Aca se hacen la validacion de los datos ingresados por el usuario
+# y se definen las funciones que se utilizan para validar los datos
+# y los tipos de datos que se ingresan por teclado
+
+
+
+# Validación de entrada para la conversión binario a decimal 
+
+
+
+
+
+
+
+
+
+
+
+# Validación de entrada para la conversión decimal a binario
