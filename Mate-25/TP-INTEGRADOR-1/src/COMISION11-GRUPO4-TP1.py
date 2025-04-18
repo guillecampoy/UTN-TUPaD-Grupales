804decfca81ddc8ba7f60dee9a7c4a156fe7fc10
Guillermo Campoy <guillecampoy@gmail.com>
2025-04-18 19:14:06 -0300

    Ajustes menores Se elimina archivo que no se utilizó (base de entrega con comisión) Se renombra archivo interfaz.py por main.py para denotar cual es el punto de partida de la ejecución Se re nombró el main.py como archivo de pruebas de funcionalidad de conversion de binario a decimal

--- Mate-25/TP-INTEGRADOR-1/src/COMISION11-GRUPO4-TP1.py
+++
@@ -1,3 +0,0 @@
-# File base a modificar para el ejercicio 1
-# Encabezado para detallar problema seleccionado
-# Ref: https://tup.sied.utn.edu.ar/course/view.php?id=16&section=18
--- Mate-25/TP-INTEGRADOR-1/src/main.py
+++ Mate-25/TP-INTEGRADOR-1/src/main.py
@@ -1,107 +1,35 @@
+# Seleccioná la operación
+# Ingresa el numero que queres convertir
+# validar datos.
+
+import validaciones
 import Conversion_binario_decimal
+import convertidor
 
-# Ejemplo de uso de la función binario_a_decimal
-# Se proponen una serie de entradas válidas e inválidas para probar la función
+# Seleccionar la operación
+print("------------------------------------------")
+print("TP INTEGRADOR 1: CONVERSOR BINARIO/DECIMAL")
+print("------------------------------------------ \n")
+print("Selecciona la operación que desees realizar")
+print(" --> A para convertir de decimal a binario")
+print(" --> B para convertir de binario a decimal.")
 
-# Ejemplos usando la función binario_a_decimal con uso de la función int()
-print("==================================================================================")
-print("Ejemplo de uso de la función binario_a_decimal (uso función int())")
-print("==================================================================================")
+# Validar que las opciones ingresadas sean correctas.
+# Se pide ingresar el número que se desea convertir.
+try:
+    opcion = input("opcion: ")
+    validaciones.validar_opcion(opcion)
+    if opcion in ("a", "A"):
+        dec_a_bin = input("Ingresá el número decimal que queres convertir: ")
+        validaciones.validar_decimal(dec_a_bin)
+        valor_bin = convertidor.decimal_binario(dec_a_bin)
+        print(f"El número decimal {dec_a_bin} convertido a binario es: {valor_bin}")
 
-#Entradas válidas
-print("-------------------")
-print("Entradas VALIDAS")
-print("-------------------")
+    if opcion in ("b", "B"):
+        bin_a_dec = input("Ingresá el número binario que queres convertir: ")
+        validaciones.validar_binario(bin_a_dec)
+        valor_dec = Conversion_binario_decimal.binario_a_decimal_alternativo(bin_a_dec)
+        print(f"El número binario {bin_a_dec} convertido a decimal es: {valor_dec}") 
+except ValueError as e:
+    print(e)
 
-# Entrada válida 1
-binario_valido1 = "1101010"
-binario_convertido1 = Conversion_binario_decimal.binario_a_decimal(binario_valido1)
-print(f"El número binario {binario_valido1} en decimal es: {binario_convertido1}")
-print()
-
-# Entrada válida 2
-binario_valido2 = "1101010000"
-binario_convertido2 = Conversion_binario_decimal.binario_a_decimal(binario_valido2)
-print(f"El número binario {binario_valido2} en decimal es: {binario_convertido2}")
-print()
-
-# Entrada válida 3
-binario_valido3 = "1110001101010"
-binario_convertido3 = Conversion_binario_decimal.binario_a_decimal(binario_valido3)
-print(f"El número binario {binario_valido3} en decimal es: {binario_convertido3}")
-print()
-
-#Entradas INVALIDAS
-print("-------------------")
-print("Entradas INVALIDAS")
-print("-------------------")
-# Entrada inválida 1
-binario_invalido1 = "HOLA"
-binario_invalido_convertido1 = Conversion_binario_decimal.binario_a_decimal(binario_invalido1)
-print(f"El número binario {binario_invalido1} en decimal es: {binario_invalido_convertido1}")
-print()
-
-# Entrada invalida 2
-binario_invalido2 = "333333"
-binario_invalido_convertido2 = Conversion_binario_decimal.binario_a_decimal(binario_invalido2)
-print(f"El número binario {binario_invalido2} en decimal es: {binario_invalido_convertido2}")
-print()
-
-# Entrada invalida 3
-binario_invalido3 = "?78734TESTS"
-binario_invalido_convertido3 = Conversion_binario_decimal.binario_a_decimal(binario_invalido3)
-print(f"El número binario {binario_invalido3} en decimal es: {binario_invalido_convertido3}")
-print()
-
-
-
-print("==================================================================================")
-print("Ejemplo de uso de la función binario_a_decimal_alternativo (sin uso función int())")
-print("==================================================================================")
-# Ejemplo de uso de la función binario_a_decimal_alternativo sin uso de la función int()
-# Se proponen las MISMAS entradas para validad integridad de ambas propuestas
-# Entradas válidas
-print("-------------------")
-print("Entradas VALIDAS")
-print("-------------------")
-# Entrada válida 1
-binario_valido1 = "1101010"
-binario_convertido1_alter = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_valido1)
-print(f"El número binario {binario_valido1} en decimal es: {binario_convertido1}")
-print(f"Igualdad con uso de función int()", {binario_convertido1 == binario_convertido1_alter})
-print()
-
-# Entrada válida 2
-binario_valido2 = "1101010000"
-binario_convertido2_alter = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_valido2)
-print(f"El número binario {binario_valido2} en decimal es: {binario_convertido2}")
-print(f"Igualdad con uso de función int()", {binario_convertido2 == binario_convertido2_alter})
-print()
-
-# Entrada válida 3
-binario_valido3 = "1110001101010"
-binario_convertido3_alter = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_valido3)
-print(f"El número binario {binario_valido3} en decimal es: {binario_convertido3}")
-print(f"Igualdad con uso de función int()", {binario_convertido3 == binario_convertido3_alter})
-print()
-
-#Entradas INVALIDAS
-print("-------------------")
-print("Entradas INVALIDAS")
-print("-------------------")
-# Entrada inválida 1
-binario_invalido1 = "HOLA"
-binario_invalido_convertido1 = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_invalido1)
-print(f"El número binario {binario_invalido1} en decimal es: {binario_invalido_convertido1}")
-print()
-
-# Entrada invalida 2
-binario_invalido2 = "333333"
-binario_invalido_convertido2 = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_invalido2)
-print(f"El número binario {binario_invalido2} en decimal es: {binario_invalido_convertido2}")
-print()
-
-# Entrada invalida 3
-binario_invalido3 = "?78734TESTS"
-binario_invalido_convertido3 = Conversion_binario_decimal.binario_a_decimal_alternativo(binario_invalido3)
-print(f"El número binario {binario_invalido3} en decimal es: {binario_invalido_convertido3}")
